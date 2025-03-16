c30aaa4ea7bcd407130afa6488773861088da29f
Ditzchann <79409183+Ditzchann@users.noreply.github.com>
2025-03-16 15:31:39 +0800

    Add remark attribute to Person

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,41 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's address in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Remark {
+
+    public static final String MESSAGE_CONSTRAINTS = "Remarks can take any value.";
+
+    public final String value;
+
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        return (other instanceof Remark) && value.equals(((Remark) other).value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
+
