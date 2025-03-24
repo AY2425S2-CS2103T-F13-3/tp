77b37dbf084834197a686348c0ec43cd3b4fef05
nat-ong555 <natalieong555@gmail.com>
2025-03-24 22:31:59 +0800

    Change testcase for LogicManagerTest

--- src/test/java/hirehive/address/logic/LogicManagerTest.java
+++ src/test/java/hirehive/address/logic/LogicManagerTest.java
@@ -56,8 +56,8 @@ public class LogicManagerTest {
 
     @Test
     public void execute_commandExecutionError_throwsCommandException() {
-        String deleteCommand = "delete 9";
-        assertCommandException(deleteCommand, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        String deleteCommand = "delete n/none";
+        assertCommandException(deleteCommand, Messages.MESSAGE_NO_SUCH_PERSON);
     }
 
     @Test
