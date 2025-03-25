aab07baf985ea89e9ec507fc8cda26d10841f769
m0destly <e1121889@u.nus.edu>
2025-03-25 12:13:08 +0800

    Update Person comment

--- src/main/java/hirehive/address/model/person/Person.java
+++ src/main/java/hirehive/address/model/person/Person.java
@@ -11,7 +11,7 @@ import hirehive.address.model.tag.Tag;
 
 /**
  * Represents a Person in the address book.
- * Guarantees: details are present, field values are validated, immutable.
+ * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Person {
 
