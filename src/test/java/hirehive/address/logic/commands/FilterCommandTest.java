dae6e8a823cbef4fc3a2575eb7751b47f64196e3
Ditzchann <79409183+Ditzchann@users.noreply.github.com>
2025-03-26 15:44:12 +0800

    Update FilterCommandTest.java

--- src/test/java/hirehive/address/logic/commands/FilterCommandTest.java
+++ src/test/java/hirehive/address/logic/commands/FilterCommandTest.java
@@ -54,7 +54,7 @@ public class FilterCommandTest {
 
     @Test
     public void execute_zeroKeywords_noPersonFound() {
-        String expectedMessage = String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
+        String expectedMessage = Messages.MESSAGE_NO_SUCH_PERSON;
         PersonContainsTagPredicate predicate = preparePredicate("test");
         FilterCommand command = new FilterCommand(predicate);
         expectedModel.updateFilteredPersonList(predicate);
