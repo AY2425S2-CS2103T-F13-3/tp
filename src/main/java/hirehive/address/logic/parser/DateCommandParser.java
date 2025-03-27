96430c7c5a703403c34b2f72ff7fa5e475acbd12
Ditzchann <79409183+Ditzchann@users.noreply.github.com>
2025-03-27 16:16:06 +0800

    Update DateCommandParser.java

--- src/main/java/hirehive/address/logic/parser/DateCommandParser.java
+++ src/main/java/hirehive/address/logic/parser/DateCommandParser.java
@@ -31,6 +31,7 @@ public class DateCommandParser implements Parser<DateCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DateCommand parse(String args) throws ParseException {
+        /*
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(
@@ -46,9 +47,8 @@ public class DateCommandParser implements Parser<DateCommand> {
 
         EditCommand.EditPersonDescriptor editPersonDescriptor = new EditCommand.EditPersonDescriptor();
 
-        //parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
-
+        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
+         */
         return new DateCommand();
     }
-
 }
