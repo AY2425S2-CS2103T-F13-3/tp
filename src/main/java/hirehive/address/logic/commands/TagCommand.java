a8880b49cef1c81c89d0f3574150bc51e4538d89
zeotheburrito <wongzenwei225@gmail.com>
2025-04-03 19:56:19 +0800

    Add functionality to tag by index as well

--- src/main/java/hirehive/address/logic/commands/TagCommand.java
+++ src/main/java/hirehive/address/logic/commands/TagCommand.java
@@ -4,10 +4,12 @@ import static hirehive.address.logic.Messages.MESSAGE_MULTIPLE_PEOPLE_QUERIED;
 import static hirehive.address.logic.commands.EditCommand.createEditedPerson;
 import static hirehive.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static hirehive.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 
+import hirehive.address.commons.core.index.Index;
 import hirehive.address.logic.Messages;
 import hirehive.address.logic.commands.EditCommand.EditPersonDescriptor;
 import hirehive.address.logic.commands.exceptions.CommandException;
@@ -17,21 +19,28 @@ import hirehive.address.model.Model;
 import hirehive.address.model.person.Person;
 
 /**
- * Tags a person identified using their name.
+ * Tags a person identified using their name or by index.
  */
 public class TagCommand extends Command {
 
     public static final String COMMAND_WORD = "tag";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Tags the person identified by the given name.\n"
-            + "Parameters: " + PREFIX_NAME + "NAME "
+            + ": Tags the person identified by the given name or index.\n"
+            + "Parameters:\n"
+            + " - " + PREFIX_NAME + "NAME "
             + PREFIX_TAG + "TAG\n"
-            + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + "John " + PREFIX_TAG + "Applicant";
+            + " - " + "INDEX (must be a positive integer) "
+            + PREFIX_TAG + "TAG\n"
+            + "Example:\n"
+            + " - " + COMMAND_WORD + " " + PREFIX_NAME + "John " + PREFIX_TAG + "Applicant\n"
+            + " - " + COMMAND_WORD + " 1 " + PREFIX_TAG + "Applicant\n";
 
+    public static final String MESSAGE_TAG_INVALID_PARAMS = "The given Tag parameters are invalid.";
     public static final String MESSAGE_TAG_PERSON_SUCCESS = "Tagged Person: %1$s";
 
-    private final NameQuery query;
+    private NameQuery query = null;
+    private Index index = null;
     private final EditPersonDescriptor editPersonDescriptor;
 
     /**
@@ -46,25 +55,49 @@ public class TagCommand extends Command {
         this.editPersonDescriptor = editPersonDescriptor;
     }
 
+    public TagCommand(Index index, EditCommand.EditPersonDescriptor editPersonDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editPersonDescriptor);
+
+        this.index = index;
+        this.editPersonDescriptor = editPersonDescriptor;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        List<Person> personToTag;
-        try {
-            personToTag = query.query(model);
-        } catch (QueryException qe) {
-            throw new CommandException(qe.getMessage());
+        if (!isNull(query)) {
+            List<Person> personToTag;
+            try {
+                personToTag = query.query(model);
+            } catch (QueryException qe) {
+                throw new CommandException(qe.getMessage());
+            }
+
+            if (personToTag.size() > 1) {
+                throw new CommandException(MESSAGE_MULTIPLE_PEOPLE_QUERIED);
+            }
+
+            Person taggedPerson = createEditedPerson(personToTag.get(0), editPersonDescriptor);
+
+            model.setPerson(personToTag.get(0), taggedPerson);
+            return new CommandResult(String.format(MESSAGE_TAG_PERSON_SUCCESS, Messages.format(taggedPerson)));
         }
+        if (!isNull(index)) {
+            List<Person> lastShownList = model.getFilteredPersonList();
 
-        if (personToTag.size() > 1) {
-            throw new CommandException(MESSAGE_MULTIPLE_PEOPLE_QUERIED);
+            if (index.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+
+            Person personToTag = lastShownList.get(index.getZeroBased());
+            Person taggedPerson = createEditedPerson(personToTag, editPersonDescriptor);
+
+            model.setPerson(personToTag, taggedPerson);
+            return new CommandResult(String.format(MESSAGE_TAG_PERSON_SUCCESS, Messages.format(taggedPerson)));
         }
-
-        Person taggedPerson = createEditedPerson(personToTag.get(0), editPersonDescriptor);
-
-        model.setPerson(personToTag.get(0), taggedPerson);
-        return new CommandResult(String.format(MESSAGE_TAG_PERSON_SUCCESS, Messages.format(taggedPerson)));
+        throw new CommandException(MESSAGE_TAG_INVALID_PARAMS);
     }
 
     @Override
