4ec760dc29123b2d8e9b34aa1f7fc641a7116d7f
Deanna <e1157414@u.nus.edu>
2025-04-03 17:19:20 +0800

    Refactor NoteCommand

---
+++ src/test/java/hirehive/address/logic/parser/DisplayNoteCommandParserTest.java
@@ -0,0 +1,28 @@
+package hirehive.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+
+import hirehive.address.logic.Messages;
+import hirehive.address.logic.commands.DisplayNoteCommand;
+import hirehive.address.logic.commands.queries.NameQuery;
+import hirehive.address.model.person.NameContainsKeywordsPredicate;
+
+public class DisplayNoteCommandParserTest {
+
+    private DisplayNoteCommandParser parser = new DisplayNoteCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsNoteCommand() {
+        DisplayNoteCommand expectedCommand = new DisplayNoteCommand(
+                new NameQuery(new NameContainsKeywordsPredicate("Alice"))
+        );
+
+        CommandParserTestUtil.assertParseSuccess(parser, "displaynote n/Alice", expectedCommand);
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        CommandParserTestUtil.assertParseFailure(parser, "displaynote Alice",
+                String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, DisplayNoteCommand.MESSAGE_USAGE));
+    }
+}
