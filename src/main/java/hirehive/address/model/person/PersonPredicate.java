79aa78bb8626ce58738eeabd672b587f144c5025
Ditzchann <79409183+Ditzchann@users.noreply.github.com>
2025-04-03 15:16:10 +0800

    Update PersonPredicate.java

--- src/main/java/hirehive/address/model/person/PersonPredicate.java
+++ src/main/java/hirehive/address/model/person/PersonPredicate.java
@@ -2,6 +2,9 @@ package hirehive.address.model.person;
 
 import java.util.function.Predicate;
 
+/**
+ * Interface used for predicates that operate on the Person class
+ */
 public interface PersonPredicate extends Predicate<Person> {
     public String getSuccessString();
 }
