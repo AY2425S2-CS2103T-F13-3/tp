e388e76939e61bc22557124174ea87983c363b45
Ditzchann <79409183+Ditzchann@users.noreply.github.com>
2025-04-04 00:09:01 +0800

    Update ScheduleCommandTest.java

--- src/test/java/hirehive/address/logic/commands/ScheduleCommandTest.java
+++ src/test/java/hirehive/address/logic/commands/ScheduleCommandTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -28,7 +31,7 @@ public class ScheduleCommandTest {
     private Model model = new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_validPerson_tagChange_success() {
+    public void execute_validApplicantAndDate_success() {
         Person editedPerson = new PersonBuilder(TypicalPersons.ALICE).withDate("31/12/2025").withTag("INTERVIEWEE").build();
         EditCommand.EditPersonDescriptor descriptor =
                 new EditPersonDescriptorBuilder(editedPerson).build();
@@ -50,7 +53,7 @@ public class ScheduleCommandTest {
     }
 
     @Test
-    public void execute_validPerson_noTagChange_success() {
+    public void execute_validIntervieweeAndDate_success() {
         Person editedPerson = new PersonBuilder(TypicalPersons.CARL).withDate("31/12/2025").build();
         EditCommand.EditPersonDescriptor descriptor =
                 new EditPersonDescriptorBuilder(editedPerson).build();
@@ -72,6 +75,32 @@ public class ScheduleCommandTest {
     }
 
     @Test
+    public void execute_validApplicantWithoutDate_success() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu")
+                .withResolverStyle(ResolverStyle.STRICT);
+        Person editedPerson = new PersonBuilder(TypicalPersons.ALICE)
+                .withDate(LocalDate.now().plusDays(1).format(formatter))
+                .withTag("INTERVIEWEE").build();
+        EditCommand.EditPersonDescriptor descriptor =
+                new EditPersonDescriptorBuilder(editedPerson).build();
+        NameQuery nameQuery = new NameQuery(new NameContainsKeywordsPredicate(TypicalPersons.ALICE.getName().fullName));
+        ScheduleCommand scheduleCommand = new ScheduleCommand(nameQuery);
+
+        String expectedMessage = String.format(ScheduleCommand.MESSAGE_DATE_PERSON_SUCCESS, Messages.format(editedPerson));
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        List<Person> personsToAddNote;
+        try {
+            personsToAddNote = nameQuery.query(expectedModel);
+            expectedModel.setPerson(personsToAddNote.get(0), editedPerson);
+        } catch (QueryException e) {
+            fail();
+        }
+
+        assertCommandSuccess(scheduleCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
     public void execute_invalidTag_throwsCommandException() {
         EditCommand.EditPersonDescriptor descriptor =
                 new EditPersonDescriptorBuilder().build();
@@ -79,6 +108,16 @@ public class ScheduleCommandTest {
         ScheduleCommand scheduleCommand = new ScheduleCommand(nameQuery, descriptor);
         assertThrows(CommandException.class, () -> scheduleCommand.execute(model), String.format(ScheduleCommand.MESSAGE_INVALID_PERSON,
                 TypicalPersons.BENSON.getName().fullName, TypicalPersons.BENSON.getTag().getTagName()));
+    }
+
+    @Test
+    public void execute_invalidName_throwsCommandException() {
+        String name = "Nonexistent";
+        EditCommand.EditPersonDescriptor descriptor =
+                new EditPersonDescriptorBuilder().build();
+        NameQuery nameQuery = new NameQuery(new NameContainsKeywordsPredicate(name));
+        ScheduleCommand scheduleCommand = new ScheduleCommand(nameQuery, descriptor);
+        assertThrows(CommandException.class, () -> scheduleCommand.execute(model), Messages.MESSAGE_NO_SUCH_PERSON);
     }
 
     @Test
